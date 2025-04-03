5a80a09de6bdef9188007d9a1db0584bb9f1254f
James Mccafferty Devers - s00236260 <s00236260@atu.ie>
2025-04-03 09:02:28 +0100

    Add project files.

---
+++ Rad302ConsoleApp2025/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tracker.WebAPIClient;
+
+namespace Rad302ConsoleApp2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ActivityAPIClient.Track(StudentID: "s00236260", StudentName: "James Mccafferty Devers", activityName: "Rad302 fe March 2025", Task: "Q1e Implementing Console queries");
+
+        }
+    }
+}
